d0e8d26a779bfc36a4955ddbe3bea8c21d0dc683
moeini <f.moein.n@gmail.com>
2022-05-23 16:50:50 +0430

    end of 18

--- account/views.py
+++ account/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
+from home.models import Post
 
 
 class UserRegisterView(View):
@@ -71,4 +72,7 @@ class UserLogoutView(LoginRequiredMixin, View):
 class UserProfileView(LoginRequiredMixin,View):
     def get(self,request, user_id):
         user = User.objects.get(id = user_id)
-        return render(request, 'account/profile.html', {'user': user})
+        posts = Post.objects.filter( user = user)
+        return render(request, 'account/profile.html', {'user': user, 'post':posts})
+
+
--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(max_length= 500)
+    slug = models.SlugField()
+    created = models.DateTimeField(auto_now_add=True)  # auto_now_add: for first time dating
+    updated = models.DateTimeField(auto_now=True)      # auto_now: Each time it was saved
+
+    def __str__(self):
+        return f' {self.slug} - {self.updated}'
